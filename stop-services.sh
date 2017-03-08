720cd20f16ae3ca0092ec59bea50e2839c6d7134
Henry Seurer <henryse@gmail.com>
2017-03-07 22:10:08 -0800

    Fixed bugs with regard to ports.

--- stop-services.sh
+++ stop-services.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #**********************************************************************
 #    Copyright (c) 2017 Henry Seurer
 #
@@ -24,25 +25,38 @@
 #
 #**********************************************************************
 
-#!/usr/bin/env bash
+# Set some pretty colors...
+#
+green='\033[1;32m'
+dark_green='\033[0;32m'
+white='\033[1;37m'
+nocolor='\033[0m'
+
 # Find the IP Address of the VM running docker.
 #
-export DOCKER_IP="0.0.0.0"
 
-if hash docker-machine 2>/dev/null; then
+if hash docker-machine env default 2>/dev/null; then
     eval $(docker-machine env default)
-    DOCKER_IP=$(docker-machine ip default)
+    export DOCKER_IP=$(docker-machine ip default)
 else
     if [ "$(uname)" == "Darwin" ]; then
-       DOCKER_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
+       export DOCKER_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-       DOCKER_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+       export DOCKER_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    else
+       export DOCKER_IP="127.0.0.1"
     fi
+fi
+
+if ["${DOCKER_IP}" == ""]; then
+    export DOCKER_IP="127.0.0.1"
 fi
 
 # We need to get the password:
 #
 export PASSWORD_JSON=$(cat password.json);
+
+echo -e "${white}Docker host is ${green}${DOCKER_IP}${nocolor}"
 
 docker-compose stop
 docker-compose rm -f
